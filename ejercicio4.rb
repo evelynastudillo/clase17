75f1bb9b5ddd41077abd9985470b5a2a0a4ed9d6
evelyn <astudillobarra.evelyn@gmail.com>
2018-06-26 22:02:08 -0400

    exercise4

--- ejercicio4.rb
+++ ejercicio4.rb
@@ -1,1 +1,17 @@
-#propiedades = {nombre: 'Beethoven', raza: 'San Bernardo', color: 'Café'}
+class Dog
+attr_accessor :nombre
+
+def initialize(data)
+  @nombre = data[:nombre]
+end
+
+def ladrar
+  puts "{#{@nombre} está ladrando}"
+
+end
+end
+
+
+
+ propiedades = {nombre: 'Beethoven', raza: 'San Bernardo', color: 'Café'}
+  p = Dog.new(propiedades)
